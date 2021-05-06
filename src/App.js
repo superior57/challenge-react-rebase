374a3c57411b8a8fa376527d861f1fdac792e64b
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:09:10 -0500

    feat: add Biology Today article

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import BiologyToday from './components/biologyToday'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           My really cool blog site
         </p>
       </header>
+      <BiologyToday/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders My really cool blog site logo', () => {
   const linkElement = screen.getByText(/My really cool blog site/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Biology Today article', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Biology Today:/i);
+  expect(linkElement).toBeInTheDocument();
+});
